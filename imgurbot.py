71c65fed9a7fa762d20890c8c3d35604703e3924
George Glessner <glessneg@mail.gvsu.edu>
2018-01-31 23:26:30 -0500

    Validate subreddit

--- imgurbot.py
+++ imgurbot.py
@@ -4,6 +4,8 @@ import praw
 from credentials import *
 import requests
 import urllib
+from prawcore import NotFound
+import sys
 
 # connect to reddit
 reddit = praw.Reddit(client_id=ID,
@@ -13,7 +15,16 @@ reddit = praw.Reddit(client_id=ID,
                      username=USERNAME)
 
 # obtain reddit to download images from, and number of images to download
+subreddit_exists = True
 subreddit = str(raw_input("Please enter subredit: "))
+
+# check that subreddit exists
+try:
+    reddit.subreddits.search_by_name(subreddit, exact=True)
+except NotFound:
+    print "Subreddit %s does not exist." % subreddit
+    sys.exit(0)
+
 num_pics = int(raw_input("Please enter number of pics: "))
 count = 0
 
@@ -22,11 +33,11 @@ for submission in reddit.subreddit(subreddit).hot():
     if count < num_pics:
         if 'https://i.imgur.com/' in submission.url:
             img_url = submission.url
-            extension = img_url.rsplit('.',1)
+            extension = img_url.rsplit('.', 1)
             extension = extension[1]
-            if extension in ["jpg","gif","jpeg","png"]:
-                img = urllib.urlretrieve(img_url, "%s%i.%s" % (subreddit, count, extension))
+            if extension in ["jpg", "gif", "jpeg", "png"]:
+                img = urllib.urlretrieve(
+                    img_url, "%s%i.%s" % (subreddit, count, extension))
                 count += 1
 
-#TODO: create better file naming mechanism
-#TODO: Implement for specified number of images to download
+# TODO: create better file naming mechanism
