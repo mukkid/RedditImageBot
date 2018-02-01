0c327d1fdfe7a5caee263b7b458dd57d9cec8820
George Glessner <glessneg@mail.gvsu.edu>
2018-01-31 22:58:51 -0500

    Initial

---
+++ README.md
@@ -0,0 +1,22 @@
+# Reddit Imgur Bot
+
+A reddit bot that downloads Imgur pictures from a given subreddit.
+
+___NOTICE: This application is functional, however I will be adding more features to it.___
+
+## How to use  
+First, create a reddit personal use script application.
+
+Next, add a `credentials.py` to your working directory and add your applications credentials.
+
+    ID='YOUR_ID'  
+    SECRET='YOUR_SECRET'  
+    PASSWORD='YOUR_PASSWORD'  
+    AGENT='Example Bot by /u/example_bot'  
+    USERNAME='YOUR_USERNAME'  
+
+Simply run the application using `python imgurbot.py` and follow the instructions.
+
+Your images will appear in your working directory.
+
+Please e-mail or comment any features that would improve this application.
---
+++ imgurbot.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import praw
+from credentials import *
+import requests
+import urllib
+
+# connect to reddit
+reddit = praw.Reddit(client_id=ID,
+                     client_secret=SECRET,
+                     password=PASSWORD,
+                     user_agent=AGENT,
+                     username=USERNAME)
+
+# obtain reddit to download images from, and number of images to download
+subreddit = str(raw_input("Please enter subredit: "))
+num_pics = int(raw_input("Please enter number of pics: "))
+count = 0
+
+# find images/gifs in subreddit
+for submission in reddit.subreddit(subreddit).hot():
+    if count < num_pics:
+        if 'https://i.imgur.com/' in submission.url:
+            img_url = submission.url
+            extension = img_url.rsplit('.',1)
+            extension = extension[1]
+            if extension in ["jpg","gif","jpeg","png"]:
+                img = urllib.urlretrieve(img_url, "%s%i.%s" % (subreddit, count, extension))
+                count += 1
+
+#TODO: create better file naming mechanism
+#TODO: Implement for specified number of images to download
